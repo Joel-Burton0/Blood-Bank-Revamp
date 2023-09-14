da33749d4990e31c6e088ba5700fa13fb50ba87e
BullyB33f <glassesdaniel@gmail.com>
2023-09-13 22:59:17 -0500

    unfinished hero section

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// header scripting
+function mouseOver(){
+    document.getElementById("hero-list2").style.visibility = "visible";
+}
+function mouseOut(){
+    document.getElementById("hero-list2").style.visibility = "hidden";
+}
+function linklist1(){
+    document.getElementById('link-item1').innerHTML = "Eligibility";
+    document.getElementById('link-item2').innerHTML = "The Process";
+    document.getElementById('link-item3').innerHTML = "Blood Safety";
+    document.getElementById('link-item4').innerHTML = "Blood Types";
+}
+function linklist2(){
+    document.getElementById('link-item1').innerHTML = "Testimonials";
+    document.getElementById('link-item2').innerHTML = "News";
+    document.getElementById('link-item3').innerHTML = "Photo Gallery";
+    document.getElementById('link-item4').innerHTML = "Videos";
+}
+
+function rotator1(){
+    document
+}
+// end of header scripting
